db0c9aa5af8844ce6eab3ec663b0632367a4829f
ketoprom <32978578+ketoprom@users.noreply.github.com>
2021-01-04 13:22:39 +0100

    Roles
    Added Roles module, default Admin, Moderator and User role, default accounts for each role

---
+++ Libre/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Libre.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Libre.Areas.Identity.IdentityHostingStartup))]
+namespace Libre.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Libre/Startup.cs
+++ Libre/Startup.cs
@@ -6,6 +6,8 @@ using Libre.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System;
 
 namespace Libre
 {
@@ -24,14 +26,23 @@ namespace Libre
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                 .AddDefaultUI()
+                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddAuthorization(options => {
+                options.AddPolicy("readpolicy",
+                    builder => builder.RequireRole("Admin", "Moderator", "User"));
+                options.AddPolicy("writepolicy",
+                    builder => builder.RequireRole("Admin", "Moderator"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +63,8 @@ namespace Libre
             app.UseAuthentication();
             app.UseAuthorization();
 
+            CreateRoles(serviceProvider).Wait();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -59,6 +72,89 @@ namespace Libre
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            Task<IdentityResult> roleResult;
+            string adminEmail = "admin@libre.com";
+            string moderatorEmail = "moderator@libre.com";
+            string userEmail = "user@libre.com";
+
+            //Check that there is an Admin role and create if not
+            Task<bool> hasAdminRole = roleManager.RoleExistsAsync("Admin");
+            hasAdminRole.Wait();
+
+            if (!hasAdminRole.Result)
+            {
+                roleResult = roleManager.CreateAsync(new IdentityRole("Admin"));
+                roleResult.Wait();
+            }
+
+            //Check that there is an Moderator role and create if not
+            Task<bool> hasModeratorRole = roleManager.RoleExistsAsync("Moderator");
+            hasModeratorRole.Wait();
+
+            if (!hasModeratorRole.Result)
+            {
+                roleResult = roleManager.CreateAsync(new IdentityRole("Moderator"));
+                roleResult.Wait();
+            }
+
+            //Check that there is an User role and create if not
+            Task<bool> hasUserRole = roleManager.RoleExistsAsync("User");
+            hasUserRole.Wait();
+
+            if (!hasUserRole.Result)
+            {
+                roleResult = roleManager.CreateAsync(new IdentityRole("User"));
+                roleResult.Wait();
+            }
+
+            //Check if the admin user exists and create it if not
+            //Add to the Admin role
+
+            var _testAdmin = await userManager.FindByEmailAsync(adminEmail);
+            if (_testAdmin == null)
+            {
+                var user = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+                var result = await userManager.CreateAsync(user, "Admin123!");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+
+            //Check if the moderator user exists and create it if not
+            //Add to the Moderator role
+
+            var _testModerator = await userManager.FindByEmailAsync(moderatorEmail);
+            if (_testModerator == null)
+            {
+                var user = new IdentityUser { UserName = moderatorEmail, Email = moderatorEmail, EmailConfirmed = true };
+                var result = await userManager.CreateAsync(user, "Moderator123!");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Moderator");
+                }
+            }
+
+            //Check if the user exists and create it if not
+            //Add to the user role
+
+            var _testUser = await userManager.FindByEmailAsync(userEmail);
+            if (_testUser == null)
+            {
+                var user = new IdentityUser { UserName = userEmail, Email = userEmail, EmailConfirmed = true };
+                var result = await userManager.CreateAsync(user, "User123!");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "User");
+                }
+            }
+
         }
     }
 }
